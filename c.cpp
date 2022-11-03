4be07c426d6ccc3fe67dd09dcd72c1d68b70bdcf
Venom-61 <subhendukarmakar61@gmail.com>
2022-11-03 16:53:57 +0530

    Minimize the Thickness

--- c.cpp
+++ c.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -6,7 +7,37 @@ using namespace std;
 // C. Minimizing the Thickness
 
 void solve() {
-    
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) 
+        cin >> a[i];
+
+    // Prefix sum
+    vector<long long> pref(n);
+    pref[0] = a[0];
+    for(int i = 1; i < n; i++) 
+        pref[i] = pref[i - 1] + a[i];
+
+    int ans = n;
+    for(int i = 0; i < n; i++) {
+        long long sum = pref[i];
+        sum += sum;
+
+        int curr = i + 1, last = i;
+        int idx;
+        while(binary_search(pref.begin(), pref.end(), sum)) {
+            idx = lower_bound(pref.begin(), pref.end(), sum) - pref.begin();
+            sum += pref[i];
+            curr = max(curr, idx - last);
+            last = idx;
+        }
+
+        if(last == n-1) 
+            ans = min(ans, curr);
+    }
+
+    cout << ans << "\n";
 }
 
 int32_t main() {

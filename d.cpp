c3691866a450a5acaa11b97067f055192f0aab59
Subhendu Karmakar <116610837+Venom-61@users.noreply.github.com>
2022-11-07 01:43:30 +0530

    Masha and a Beautiful Tree

--- d.cpp
+++ d.cpp
@@ -5,19 +5,47 @@ using namespace std;
 
 // D. Masha and a Beautiful Tree
 
-void solve() {
+vector<int> a;
+
+int solve(int l, int r) {
+    if(r - l == 1) 
+        return 0;
     
+    int mid = (l + r) >> 1;
+    int ml = *max_element(a.begin() + l, a.begin() + mid);
+    int mr = *max_element(a.begin() + mid, a.begin() + r);
+    
+    int ans = 0;
+    if(ml > mr) {
+        ++ans;
+        for(int i = 0; i < (mid - l); ++i)
+            swap(a[l + i], a[mid + i]);   
+    }
+    
+    return solve(l, mid) + solve(mid, r) + ans;
 }
 
-int32_t main() {
+int solve() {
+    int n; cin >> n;
+    a.resize(n);
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    
+    int ans = solve(0, n);
+    if(is_sorted(a.begin(), a.end()))
+        return ans;
+    return -1;
+}
+
+int main() {
     ios::sync_with_stdio(false);
-
-    int tc = 1;
-    cin >> tc;
-    for(int i = 1; i <= tc; i++) {
-        // cout << "Case: #" << i << " ";
-        solve();
+    cin.tie(nullptr);
+    
+    int test = 1;
+    cin >> test;
+    while(test--) {
+        cout << solve() << "\n";
     }
-
+    
     return 0;
 }
